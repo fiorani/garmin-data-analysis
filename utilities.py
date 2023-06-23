aa0b0e8431067ef8bd496ded1c9f875be8d2357f
Mattia Panni <pnmattia@gmail.com>
2023-06-23 17:28:03 +0200

    Update simple models

---
+++ utilities.py
@@ -0,0 +1,17 @@
+# MSE e R^2 sono incluse in scikit-learn
+pip install numpy
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
+
+# definisco l'errore relativo
+def relative_error(y_true, y_pred):
+    return np.mean(np.abs((y_true - y_pred) / y_true))
+
+def print_eval(X, y, model):
+    preds = model.predict(X)
+    mse = mean_squared_error(y, preds)
+    re = relative_error(y, preds)
+    r2 = r2_score(y, preds)
+    print(f"   Mean squared error: {mse:.5}")
+    print(f"       Relative error: {re:.5%}")
+    print(f"R-squared coefficient: {r2:.5}")
