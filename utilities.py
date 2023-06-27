1f7c520d270c464c3d35ce4fd814f3f9254dde97
Mattia Panni <pnmattia@gmail.com>
2023-06-27 18:48:28 +0200

    Replace all models with grid searches and update utils

--- utilities.py
+++ utilities.py
@@ -6,11 +6,15 @@ from sklearn.metrics import mean_squared_error, r2_score
 def relative_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true))
 
-def print_eval(X, y, model):
+def rmspe(y_real, y_pred):
+    return np.sqrt(np.mean((y_pred /y_real - 1) ** 2))
+
+def print_eval(X, y, model, tree=False):
     preds = model.predict(X)
     mse = mean_squared_error(y, preds)
     re = relative_error(y, preds)
     r2 = r2_score(y, preds)
-    print(f"   Mean squared error: {mse:.5}")
-    print(f"       Relative error: {re:.5%}")
-    print(f"R-squared coefficient: {r2:.5}")
+    print(f"   Mean squared error: {mse:>12.5}")
+    print(f"       Relative error: {re:>12.5%}" if not tree else f"    RMSPE: {rmspe(y, preds):>12.5}")
+    print(f"R-squared coefficient: {r2:>12.5}")
+
