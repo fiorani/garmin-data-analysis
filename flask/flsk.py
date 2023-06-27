70c046ff5f09c3b3a5270407942af1c8c95b3eca
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-06-27 20:35:17 +0200

    add flask

--- flask/flsk.py
+++ flask/flsk.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import pickle
 
 app = Flask(__name__)
 app.debug = True
@@ -6,6 +7,17 @@ app.debug = True
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/predict")
+def predict():
+    inputs = [
+        float(request.args["LIMIT_BAL"]),
+        int(request.args["PAY_6"]),
+        int(request.args["EDUCATION"])
+    ]
+    with app.open_resource("model.bin", "rb") as f:
+        model = pickle.load(f)
+    response = model.predict([inputs])[0]
+    return render_template("predict.html", resp=response)
 
 if __name__ == '__main__':
   app.run()
