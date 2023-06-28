0bead43e93c795465ec097f4f1f6b9d352d17876
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-06-28 18:27:49 +0200

    fix flask

--- flsk.py
+++ flsk.py
@@ -5,21 +5,19 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/predict")
-def predict():
-    inputs = [
-        float(request.args["LIMIT_BAL"]),
-        int(request.args["PAY_6"]),
-        int(request.args["EDUCATION"])
-    ]
-    with app.open_resource("models/model.bin", "rb") as f:
-        model = pickle.load(f)
-    response = model.predict([inputs])[0]
-    return render_template("predict.html", resp=response)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        inputs = [
+            float(request.form["distance[m]"]),
+            float(request.form["altitude[m]"]),
+        ]
+        with app.open_resource("models/model.bin", "rb") as f:
+            model = pickle.load(f)
+        response = model.predict([inputs])[0]
+        return render_template("index.html", resp=response)
 
 if __name__ == '__main__':
   app.run()
